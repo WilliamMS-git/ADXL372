aafa02357359740dcc1559e42a453c4b4828d3b3
Fourier-git <81632519+Fourier-git@users.noreply.github.com>
2023-12-28 09:25:17 +0100

    readFifoData. Auto formatting

--- src/ADXL372.cpp
+++ src/ADXL372.cpp
@@ -1,6 +1,6 @@
 #include "ADXL372.h"
 
-#define DEVID_PRODUCT 0xFA //372 in octal :) 
+#define DEVID_PRODUCT 0xFA // 372 in octal :)
 
 // Data registers. Each axis data has a 12 bit value. Data is left justified, MSBFIRST.
 // Register *_H contains the eight most significant bits (MSBs), and Register *_L contains the four least significant bits (LSBs) of the 12-bit value
@@ -21,26 +21,28 @@
 #define STATUS 0x04 // Status register
 
 #define FIFO_SAMPLES 0x39 // FIFO samples register
-#define FIFO_CTL 0x3A // FIFO control register
+#define FIFO_CTL 0x3A     // FIFO control register
 
-#define TIMING 0x3D // Timing control register
-#define MEASURE 0x3E // Measurement control register
+#define TIMING 0x3D    // Timing control register
+#define MEASURE 0x3E   // Measurement control register
 #define POWER_CTL 0x3F // Power control register
+
+#define FIFO_DATA 0x42 // FIFO data register
 
 // System bitmasks
 #define FIFO_SAMPLES_8_MASK 0xFE // FIFO control
-#define FIFO_MODE_MASK 0xF9 
+#define FIFO_MODE_MASK 0xF9
 #define FIFO_FORMAT_MASK 0xC7
 
 #define EXT_SYNC_MASK 0xFE // Timing
 #define EXT_CLK_MASK 0xFD
-#define WAKEUP_RATE_MASK 0xE3 
+#define WAKEUP_RATE_MASK 0xE3
 #define ODR_MASK 0x1F
 
 #define BANDWIDTH_MASK 0xF8 // Measure
 #define LOW_NOISE_MASK 0xF7
 #define LINKLOOP_MASK 0xCF
-#define AUTOSLEEP_MASK 0xBF 
+#define AUTOSLEEP_MASK 0xBF
 
 #define MODE_MASK 0xFC // Power Control
 #define HPF_DISABLE_MASK 0xFB
@@ -50,8 +52,8 @@
 
 // Accelerometer Constants
 #define SPI_SPEED 10000000 // ADXL372 supports up to 10MHz in SCLK frequency
-#define SCALE_FACTOR 100 // mg per LSB
-#define MG_TO_G 0.001 // g per mg
+#define SCALE_FACTOR 100   // mg per LSB
+#define MG_TO_G 0.001      // g per mg
 
 ADXL372class::ADXL372class(int csPinInput)
 {
@@ -66,17 +68,16 @@ void ADXL372class::begin()
 {
     SPI.begin();
     SPI.beginTransaction(SPISettings(SPI_SPEED, MSBFIRST, SPI_MODE0)); // CPHA = CPOL = 0
-    pinMode(m_csPin, OUTPUT); // Setting chip select pin
-    digitalWrite(m_csPin, HIGH); // Pin ready
-
+    pinMode(m_csPin, OUTPUT);                                          // Setting chip select pin
+    digitalWrite(m_csPin, HIGH);                                       // Pin ready
 }
 
 void ADXL372class::begin(Bandwidth bandwidth, Odr odr)
 {
     SPI.begin();
     SPI.beginTransaction(SPISettings(SPI_SPEED, MSBFIRST, SPI_MODE0)); // CPHA = CPOL = 0
-    pinMode(m_csPin, OUTPUT); // Setting chip select pin
-    digitalWrite(m_csPin, HIGH); // Pin ready
+    pinMode(m_csPin, OUTPUT);                                          // Setting chip select pin
+    digitalWrite(m_csPin, HIGH);                                       // Pin ready
 
     setOdr(odr);
     setBandwidth(bandwidth);
@@ -84,12 +85,13 @@ void ADXL372class::begin(Bandwidth bandwidth, Odr odr)
 
 void ADXL372class::end()
 {
-    //set some adresses here
+    // set some adresses here
 
     SPI.end();
 }
 
-void ADXL372class::printDevice(){
+void ADXL372class::printDevice()
+{
     byte devidAd = readRegister(DEVID_AD);
     byte devidMst = readRegister(DEVID_MST);
     byte partId = readRegister(PARTID);
@@ -107,32 +109,34 @@ void ADXL372class::printDevice(){
 
     Serial.print("REVID: 0x");
     Serial.println(revId, HEX);
-    
+
     Serial.print("STATUS: 0x");
     Serial.println(status, HEX);
 }
 
-void ADXL372class::setStatusCheck(bool isCheckingStatus) {
+void ADXL372class::setStatusCheck(bool isCheckingStatus)
+{
     m_isCheckingStatus = isCheckingStatus;
 }
 
+void ADXL372class::readAcceleration(float &x, float &y, float &z)
+{
 
-void ADXL372class::readAcceleration(float& x, float& y, float& z) {
-    
     if (m_isCheckingStatus == true)
     {
         byte status;
-        do {
+        do
+        {
             status = readRegister(0x04);
-        } while((status & 0x01) == 0); // Waiting for status register
+        } while ((status & 0x01) == 0); // Waiting for status register
     }
 
     // The register is left justified. *DATA_H has bits 11:4 of the register. *DATA_L has bits 3:0.
-    short rawX = readRegister(XDATA_H) << 8 | readRegister(XDATA_L); 
+    short rawX = readRegister(XDATA_H) << 8 | readRegister(XDATA_L);
     short rawY = readRegister(YDATA_H) << 8 | readRegister(YDATA_L);
     short rawZ = readRegister(ZDATA_H) << 8 | readRegister(ZDATA_L);
 
-    rawX = rawX >> 4; 
+    rawX = rawX >> 4;
     rawY = rawY >> 4;
     rawZ = rawZ >> 4;
 
@@ -142,97 +146,131 @@ void ADXL372class::readAcceleration(float& x, float& y, float& z) {
     z = rawZ * SCALE_FACTOR * MG_TO_G;
 }
 
-void ADXL372class::setFifoSamples(int sampleSize){
-    if(sampleSize > 512){
+void ADXL372class::readFifoData(uint16_t &fifoData)
+{
+    digitalWrite(m_csPin, LOW);
+    SPI.transfer(FIFO_DATA << 1 | 1;);
+    for(int i = 0; i < sampleSize; i++) {
+        fifoData[i * 3] = SPI.transfer16(0x00);
+        fifoData[i * 3 + 1] = SPI.transfer16(0x00);
+        fifoData[i * 3 + 2] = SPI.transfer16(0x00);
+    }
+
+    digitalWrite(m_csPin, HIGH);
+}
+
+void ADXL372class::setFifoSamples(int sampleSize)
+{
+    if (sampleSize > 512)
+    {
         Serial.println("WARNING: FIFO samples limit is 512");
         sampleSize = 512;
     }
-    sampleSize -= 1; 
-    writeRegister(FIFO_SAMPLES, sampleSize & 0xFF); // Sending the 8 least significant bits in the samples 
+    m_sampleSize = sampleSize;
+    sampleSize -= 1;
+    writeRegister(FIFO_SAMPLES, sampleSize & 0xFF); // Sending the 8 least significant bits in the samples
     updateRegister(FIFO_CTL, (sampleSize > 0xFF), FIFO_SAMPLES_8_MASK);
 }
 
-void ADXL372class::setFifoMode(FifoMode mode) {
+void ADXL372class::setFifoMode(FifoMode mode)
+{
     byte modeShifted = mode << 1; // starts from bit 1 in register
     updateRegister(FIFO_CTL, modeShifted, FIFO_MODE_MASK);
 }
 
-void ADXL372class::setFifoFormat(FifoFormat format) {
+void ADXL372class::setFifoFormat(FifoFormat format)
+{
     byte formatShifted = format << 3; // starts from bit 3 in register
     updateRegister(FIFO_CTL, formatShifted, FIFO_FORMAT_MASK);
 }
 
-void ADXL372class::setOdr(Odr odr){
+void ADXL372class::setOdr(Odr odr)
+{
     m_odr = (int)odr;
-    if (m_odr < m_bandwidth){
+    if (m_odr < m_bandwidth)
+    {
         Serial.println("WARNING: ODR must be at least double the bandwidth, to not violate the Nyquist criteria. Otherwise signal integrity will not be maintained");
     }
     byte odrShifted = m_odr << 5; // odr bits start from bit 5
     updateRegister(TIMING, odrShifted, ODR_MASK);
 }
 
-void ADXL372class::setWakeUpRate(WakeUpRate wur) {
+void ADXL372class::setWakeUpRate(WakeUpRate wur)
+{
     byte wurShifted = wur << 2; // wur bits start from bit 2
     updateRegister(TIMING, wurShifted, WAKEUP_RATE_MASK);
 }
 
-void ADXL372class::enableExternalClock(bool isEnabled){
+void ADXL372class::enableExternalClock(bool isEnabled)
+{
     byte valueShifted = isEnabled << 1; // bit 1 in register
     updateRegister(TIMING, valueShifted, EXT_CLK_MASK);
 }
 
-void ADXL372class::enableExternalTrigger(bool isEnabled){
+void ADXL372class::enableExternalTrigger(bool isEnabled)
+{
     updateRegister(TIMING, isEnabled, EXT_SYNC_MASK);
 }
 
-void ADXL372class::setBandwidth(Bandwidth bandwidth){
+void ADXL372class::setBandwidth(Bandwidth bandwidth)
+{
     m_bandwidth = (int)bandwidth;
-    if (m_bandwidth > m_odr){
+    if (m_bandwidth > m_odr)
+    {
         Serial.println("WARNING: Bandwidth must be no greater than half the ODR, to not violate the Nyquist criteria. Otherwise signal integrity will not be maintained");
     }
     updateRegister(MEASURE, bandwidth, BANDWIDTH_MASK);
 }
 
-void ADXL372class::enableLowNoiseOperation(bool isEnabled){
+void ADXL372class::enableLowNoiseOperation(bool isEnabled)
+{
     byte valueShifted = isEnabled << 3; // bit 3 in register
     updateRegister(MEASURE, valueShifted, LOW_NOISE_MASK);
 }
 
-void ADXL372class::setLinkLoopActivityProcessing(LinkLoop activityProcessing){
+void ADXL372class::setLinkLoopActivityProcessing(LinkLoop activityProcessing)
+{
     byte valueShifted = activityProcessing << 2; // bit 2 in register
     updateRegister(MEASURE, valueShifted, LINKLOOP_MASK);
 }
 
-void ADXL372class::enableAutosleep(bool isEnabled){
+void ADXL372class::enableAutosleep(bool isEnabled)
+{
     byte valueShifted = isEnabled << 6; // bit 6 in register
     updateRegister(MEASURE, valueShifted, AUTOSLEEP_MASK);
 }
 
-void ADXL372class::setOperatingMode(OperatingMode opMode){
+void ADXL372class::setOperatingMode(OperatingMode opMode)
+{
     updateRegister(POWER_CTL, opMode, MODE_MASK);
 }
 
-void ADXL372class::disableHighPassFilter(bool isDisabled){
+void ADXL372class::disableHighPassFilter(bool isDisabled)
+{
     byte valueShifted = isDisabled << 2; // bit 2 in register
     updateRegister(POWER_CTL, valueShifted, HPF_DISABLE_MASK);
 }
 
-void ADXL372class::disableLowPassFilter(bool isDisabled){
+void ADXL372class::disableLowPassFilter(bool isDisabled)
+{
     byte valueShifted = isDisabled << 3; // bit 3 in register
     updateRegister(POWER_CTL, valueShifted, LPF_DISABLE_MASK);
 }
 
-void ADXL372class::setFilterSettling(FilterSettlingPeriod filterSettling){
+void ADXL372class::setFilterSettling(FilterSettlingPeriod filterSettling)
+{
     byte valueShifted = filterSettling << 4; // bit 4 in register
     updateRegister(POWER_CTL, valueShifted, LPF_DISABLE_MASK);
 }
 
-void ADXL372class::setInstantOnThreshold(InstantOnThreshold threshold){
+void ADXL372class::setInstantOnThreshold(InstantOnThreshold threshold)
+{
     byte valueShifted = threshold << 5; // bit 5 in register
     updateRegister(POWER_CTL, valueShifted, LPF_DISABLE_MASK);
 }
 
-uint8_t ADXL372class::readRegister(byte regAddress){
+uint8_t ADXL372class::readRegister(byte regAddress)
+{
     digitalWrite(m_csPin, LOW);
     regAddress = regAddress << 1 | 1; // Reading from a register
     SPI.transfer(regAddress);
@@ -241,19 +279,20 @@ uint8_t ADXL372class::readRegister(byte regAddress){
     return value;
 }
 
-void ADXL372class::writeRegister(byte regAddress, uint8_t value) {
-    digitalWrite(m_csPin, LOW); 
+void ADXL372class::writeRegister(byte regAddress, uint8_t value)
+{
+    digitalWrite(m_csPin, LOW);
     regAddress = regAddress << 1; // Writing to a register
     SPI.transfer(regAddress);
     SPI.transfer(value);
     digitalWrite(m_csPin, HIGH);
 }
 
-void ADXL372class::updateRegister(byte regAddress, uint8_t value, byte mask) {
+void ADXL372class::updateRegister(byte regAddress, uint8_t value, byte mask)
+{
     // Need to use bitmasks to only change the desired bits in the registers
     byte registerState = readRegister(regAddress);
     registerState &= mask;
     value |= registerState & ~mask;
     writeRegister(regAddress, value);
 }
-
--- src/ADXL372.h
+++ src/ADXL372.h
@@ -1,14 +1,16 @@
 #include "Arduino.h"
 #include "SPI.h"
 
-enum FifoMode {
+enum FifoMode
+{
     FIFO_DISABLED = 0b00,
     STREAM = 0b01,
     TRIGGER = 0b10,
     OLDEST_SAVED = 0b11
 };
 
-enum FifoFormat {
+enum FifoFormat
+{
     XYZ = 0b000,
     X = 0b001,
     Y = 0b010,
@@ -19,7 +21,8 @@ enum FifoFormat {
     XYZ_PEAK = 0b111
 };
 
-enum Odr {
+enum Odr
+{
     ODR_400Hz = 0b000,
     ODR_800Hz = 0b001,
     ODR_1600Hz = 0b010,
@@ -27,7 +30,8 @@ enum Odr {
     ODR_6400Hz = 0b100
 };
 
-enum WakeUpRate {
+enum WakeUpRate
+{
     WUR_52ms = 0b000,
     WUR_104ms = 0b001,
     WUR_208ms = 0b010,
@@ -38,7 +42,8 @@ enum WakeUpRate {
     WUR_24576ms = 0b111,
 };
 
-enum Bandwidth {
+enum Bandwidth
+{
     BW_200Hz = 0b000,
     BW_400Hz = 0b001,
     BW_800Hz = 0b010,
@@ -46,25 +51,29 @@ enum Bandwidth {
     BW_3200Hz = 0b100
 };
 
-enum LinkLoop {
+enum LinkLoop
+{
     DEFAULT = 0b00,
     LINKED = 0b01,
     LOOPED = 0b10
 };
 
-enum OperatingMode {
+enum OperatingMode
+{
     STANDBY = 0b00,
     WAKE_UP = 0b01,
     INSTANT_ON = 0b10,
     FULL_BANDWIDTH = 0b11
 };
 
-enum FilterSettlingPeriod {
+enum FilterSettlingPeriod
+{
     FSP_370ms = 0,
     FSP_16ms = 1
 };
 
-enum InstantOnThreshold {
+enum InstantOnThreshold
+{
     IOT_LOW_THRESH = 0,
     IOT_HIGH_THRESH = 1
 };
@@ -81,12 +90,13 @@ public:
     void printDevice();
     void setStatusCheck(bool isCheckingStatus);
 
-    void readAcceleration(float& x, float& y, float& z);
+    void readAcceleration(float &x, float &y, float &z);
 
+    void readFifoData(uint16_t &fifoData);
     void setFifoSamples(int sampleSize);
     void setFifoMode(FifoMode mode);
     void setFifoFormat(FifoFormat format);
-    
+
     void setOdr(Odr odr);
     void setWakeUpRate(WakeUpRate wur);
     void enableExternalClock(bool isEnabled);
@@ -102,16 +112,15 @@ public:
     void disableLowPassFilter(bool isDisabled);
     void setFilterSettling(FilterSettlingPeriod filterSettling);
     void setInstantOnThreshold(InstantOnThreshold threshold);
-    
+
 private:
     int m_csPin;
     int m_odr = 0;
     int m_bandwidth = 0;
     bool m_isCheckingStatus = true;
+    int m_sampleSize;
 
     uint8_t readRegister(byte regAddress);
     void writeRegister(byte regAddress, uint8_t value);
     void updateRegister(byte regAddress, uint8_t value, byte mask);
-
-
 };
